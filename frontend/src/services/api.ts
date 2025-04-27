64537f10f3c597635ae9e28dec4ca7cde2a6e1a3
Дедов Иван <johnnyded@icloud.com>
2025-04-27 20:08:56 +0300

    Some Server Updates

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -183,7 +183,7 @@ export const userApi = {
     api.post('/users/register', { telegram_id: telegramId, username }),
   getUserReferrals: (telegramId: number | string) => 
     api.get<any, any[]>(`/users/referrals/${telegramId}`),
-  registerWithReferral: (telegramId: number | string, username: string, refId: number | string) => 
+  registerWithReferral: (telegramId: number | string, username: string, refId: number) => 
     api.post('/users/register_with_referral', { 
       telegram_id: telegramId, 
       username, 
